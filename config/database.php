8452fcb2c81cd3281d4f5d133d4ad649a3a12a48
Abdullahi Kabri <kabriacid01@gmail.com>
2024-12-18 23:01:09 +0100

    Update database.php

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "fms_db";
+
+// Connect to the database
+$conn = new mysqli($servername, $username, $password, $db);
+
+if (!$conn) {
+    die("Connection failed");
+}
